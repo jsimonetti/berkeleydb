2aa0a3826ca821d73b3981cc0ef8f8a5053f5d2f
Matt Butcher <technosophos@gmail.com>
2013-06-11 22:12:46 -0500

    gofmt

--- berkeleydb/bdb.go
+++ berkeleydb/bdb.go
@@ -2,86 +2,85 @@ package berkeleydb
 
 /*
 #cgo LDFLAGS: -ldb
-#include <db.h> 
+#include <db.h>
 #include "bdb.h"
 */
 import "C"
 
 import (
-  "fmt"
+	"fmt"
 )
 
 const BDB_VERSION string = "0.0.1"
 
 // Flags for opening a database.
 const (
-  DB_CREATE = C.DB_CREATE
-  DB_EXCL = C.DB_EXCL
-  DB_RDONLY = C.DB_RDONLY
-  DB_TRUNCATE= C.DB_TRUNCATE
+	DB_CREATE   = C.DB_CREATE
+	DB_EXCL     = C.DB_EXCL
+	DB_RDONLY   = C.DB_RDONLY
+	DB_TRUNCATE = C.DB_TRUNCATE
 )
 
 // Database types.
 const (
-  DB_BTREE = C.DB_BTREE
-  DB_HASH = C.DB_HASH
-  DB_HEAP = C.DB_HEAP
-  DB_RECNO = C.DB_RECNO
-  DB_QUEUE = C.DB_QUEUE
-  DB_UNKNOWN = C.DB_UNKNOWN
+	DB_BTREE   = C.DB_BTREE
+	DB_HASH    = C.DB_HASH
+	DB_HEAP    = C.DB_HEAP
+	DB_RECNO   = C.DB_RECNO
+	DB_QUEUE   = C.DB_QUEUE
+	DB_UNKNOWN = C.DB_UNKNOWN
 )
 
 type BDB struct {
-  db *C.DB
+	db *C.DB
 }
 
 type Errno int
 
 func CreateDB() (*BDB, int) {
-  var db *C.DB
-  err := C.db_create(&db, nil, 0)
+	var db *C.DB
+	err := C.db_create(&db, nil, 0)
 
-  if err > 0 {
-    return nil, int(err)
-  }
+	if err > 0 {
+		return nil, int(err)
+	}
 
-  return &BDB{db}, 0
+	return &BDB{db}, 0
 }
 
 func (handle *BDB) Open(filename string, dbtype C.DBTYPE, flags C.u_int32_t) int {
-  db := handle.db
+	db := handle.db
 
-  ret := C.go_db_open(db, nil, C.CString(filename), nil, dbtype, flags, 0)
+	ret := C.go_db_open(db, nil, C.CString(filename), nil, dbtype, flags, 0)
 
-  return int(ret)
+	return int(ret)
 }
 
 func (handle *BDB) Close() int {
-  ret := C.go_db_close(handle.db, 0)
+	ret := C.go_db_close(handle.db, 0)
 
-  return int(ret)
+	return int(ret)
 }
 
 func (handle *BDB) OpenFlags() (C.u_int32_t, int) {
-  var flags C.u_int32_t
+	var flags C.u_int32_t
 
-  ret := C.go_db_get_open_flags(handle.db, &flags)
+	ret := C.go_db_get_open_flags(handle.db, &flags)
 
-  return flags, int(ret)
+	return flags, int(ret)
 }
 
 func (handle *BDB) Remove(filename string) int {
-  ret := C.go_db_remove(handle.db, C.CString(filename))
+	ret := C.go_db_remove(handle.db, C.CString(filename))
 
-  return int(ret)
+	return int(ret)
 }
-
 
 // UTILITY FUNCTIONS
 
 func Version() string {
-  lib_version := C.GoString(C.db_full_version(nil, nil, nil, nil, nil))
+	lib_version := C.GoString(C.db_full_version(nil, nil, nil, nil, nil))
 
-  tpl := "%s (Go bindings v%s)"
-  return fmt.Sprintf(tpl, lib_version, BDB_VERSION)
+	tpl := "%s (Go bindings v%s)"
+	return fmt.Sprintf(tpl, lib_version, BDB_VERSION)
 }
